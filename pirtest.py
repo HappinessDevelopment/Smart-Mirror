09bc509daedbe0fe331414086a81113c3460051c
jberlynn <jnguy@uw.edu>
2016-08-18 19:50:02 -0700

    Update pirtest.py

--- pirtest.py
+++ pirtest.py
@@ -7,10 +7,10 @@ GPIO.setup(3, GPIO.OUT)         #LED output pin
 while True:
        i=GPIO.input(11)
        if i==0:                 #When output from motion sensor is LOW
-             print "fucking idiot",i
+             print "print1",i
              GPIO.output(3, 0)  #Turn OFF LED
              time.sleep(0.1)
        elif i==1:               #When output from motion sensor is HIGH
-             print "hello, it's me",i
+             print "print2",i
              GPIO.output(3, 1)  #Turn ON LED
              time.sleep(0.1)
